c2354d44f7ca9fe8f375b9ad94048a4350e4bdd6
jgome047 <66091082+jgome047@users.noreply.github.com>
2021-11-30 10:54:27 -0800

    3d angle calc works
    works, trying to mesh this with Anna math

---
+++ hand angle test 3D.py
@@ -0,0 +1,118 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+import uuid
+import os
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+from matplotlib import pyplot as plt
+
+
+#define the joints of interest (pretty much all of them)
+joint_list = [[4,3,2], [8,7,6], [12,11,10], [16,15,14], [20,19,18]]
+
+joint_list[3]
+
+
+#function for calculating finger angles (for now, in 2D)
+def draw_finger_angles(image, results, joint_list):
+    
+    # Loop through hands
+    for hand in results.multi_hand_landmarks:
+        #Loop through joint sets 
+        for joint in joint_list:
+            a = np.array([hand.landmark[joint[0]].x, hand.landmark[joint[0]].y]) # First coord
+            b = np.array([hand.landmark[joint[1]].x, hand.landmark[joint[1]].y]) # Second coord
+            c = np.array([hand.landmark[joint[2]].x, hand.landmark[joint[2]].y]) # Third coord
+            
+            radians = np.arctan2(np.linalg.norm(np.cross(a-b, c-b)), np.dot(a-b, c-b))
+            angle = np.abs(radians*180.0/np.pi)
+
+            if angle > 180.0:
+                angle = 360 - angle
+
+            cv2.putText(image, str(round(angle,2)), tuple(np.multiply([b[0],b[1]], 720).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+    return image
+
+#labelling hands as L and R, angles of each finger, and confidence values
+def get_label(index, hand, results):
+    output = None
+    for idx, classification in enumerate(results.multi_handedness):
+        if classification.classification[0].index == index:
+            
+            # Process results
+            label = classification.classification[0].label
+            score = classification.classification[0].score
+            text = '{} {}'.format(label, round(score, 2))
+            
+            # Extract Coordinates
+            coords = tuple(np.multiply(
+                np.array((hand.landmark[mp_hands.HandLandmark.WRIST].x, hand.landmark[mp_hands.HandLandmark.WRIST].y)),
+            [640,480]).astype(int))
+            
+            output = text, coords
+            
+    return output
+
+#camera capture and drawing of finger segments
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands: 
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # BGR 2 RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        # Flip on horizontal
+        image = cv2.flip(image, 1)
+        
+        # Set flag
+        image.flags.writeable = False
+        
+        # Detections
+        results = hands.process(image)
+        
+        # Set flag to true
+        image.flags.writeable = True
+        
+        # RGB 2 BGR
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Detections
+        print(results)
+        
+        # Rendering results
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS, 
+                                        mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                        mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+                                         )
+                
+                # Render left or right detection
+                if get_label(num, hand, results):
+                    text, coord = get_label(num, hand, results)
+                    cv2.putText(image, text, coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            
+            # Draw angles to image from joint list
+            draw_finger_angles(image, results, joint_list)
+            
+        # Save our image    
+        #cv2.imwrite(os.path.join('Output Images', '{}.jpg'.format(uuid.uuid1())), image)
+        cv2.imshow('Hand Tracking', image)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+cv2.waitKey(1)
+
+
+
